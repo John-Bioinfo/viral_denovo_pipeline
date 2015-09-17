c5c50b250bd831f0009414539e065e2e6d66aeb4
smorfopoulou <sofia.morfopoulou@gmail.com>
2015-09-17 15:31:11 +0100

    1) Add Picard step for removing duplicates based on mapping coord. 2) Call consensus with QUASR 3) Removed EBV flag, use continuous instead for EBV data 4) updated README

--- example_quick/submit_pipeline_example.sh
+++ example_quick/submit_pipeline_example.sh
@@ -1,5 +1,5 @@
 ### Specify where the main pipeline file is (in general use full paths)
-pipeline=/home/ucbtsmo/Projects/viral_denovo_pipeline/scripts/Morfopoulou_pipeline_viral_LEGION_v4.sh
+pipeline=/home/ucbtsmo/Projects/viral_denovo_pipeline/scripts/Morfopoulou_pipeline_viral_LEGION_v5.sh
 
 
 ### Specify your working directory
@@ -21,14 +21,15 @@ inputFiles="2 /home/ucbtsmo/Projects/viral_denovo_pipeline/example_quick/rawData
 
 scriptStep1=${outDir}/cluster/submission/step.sh
  dbViral=/home/ucbtsmo/Projects/viral_denovo_pipeline/blast_databases/EBV_blastdb/EBV_124.fasta   
- dbViral_single=/home/ucbtsmo/Projects/viral_denovo_pipeline/blast_databases/EBV_single_db/
+ dbViral_single=/home/ucbtsmo/Projects/viral_denovo_pipeline/blast_databases/EBV_single_db/EBV.fasta
 
 
-genome=EBV #### set value to continuous for all pathogens except for ebv (value needs to be EBV) or  fluA (value needs to be FLUA)
+genome=continuous #### set value to continuous for all pathogens that have non-segmented genome (i.e this flag should now be used for EBV as well) or  fluA (value needs to be FLUA)
 instrument=NextSeq    ### can change to HiSeq or MiSeq 
 nthreads=4               ### number of processors
+collapseLev=72
 
-bash ${pipeline} --script ${scriptStep1} --inputFiles ${inputFiles}  --outDir ${outDir} --sample ${sample} --db ${dbViral} --dbSingle ${dbViral_single}  --type ${instrument}  --genome ${genome} --nthreads ${nthreads}    ### collapsing, merging, trimming,blastn, assembly, pseudogenome, variants                                                                                                                                      
+bash ${pipeline} --script ${scriptStep1} --inputFiles ${inputFiles}  --outDir ${outDir} --sample ${sample} --db ${dbViral} --dbSingle ${dbViral_single}  --type ${instrument}  --genome ${genome} --nthreads ${nthreads} --collapseLev ${collapseLev}    ### collapsing, merging, trimming,blastn, assembly, pseudogenome, variants                                                                                                                                      
 
 
 
